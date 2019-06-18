9e19fef851d6e3600401160c23252f9538e6d09c
wens07 <wens.wq@gmail.com>
2019-06-18 20:17:57 +0800

    add build related

---
+++ background/produce.sh
@@ -0,0 +1,5 @@
+browserify origin_inpage.js > extension/inpage.js
+browserify -t brfs origin_contentscript.js > extension/contentscript.js
+# browserify origin_lib.js > extension/lib.js
+# # browserify origin_ui.js > extension/ui.js
+browserify origin_background.js > extension/background.js
