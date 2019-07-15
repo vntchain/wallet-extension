25537b0874721b3d09ddad14d7b1db1e5888ba54
Yan Feng Xi <yanfx@yunphant.com>
2019-07-15 16:54:40 +0800

    Change config

--- src/constants/net.js
+++ src/constants/net.js
@@ -2,6 +2,6 @@ export const netList = ['', '主网', '测试网']
 
 export const netUrlList = [
   '',
-  'https://hubscan.vnt.link', //主网
+  'https://scan.vntchain.io', //主网
   'https://hubscan.vnt.link' //测试网
 ]
