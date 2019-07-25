b4df3cca9b7e1a3139ffe869b589c86766227477
zhouxs <zhouxs@yunphant.com>
2019-07-25 14:12:17 +0800

    fix: addr can use capital letter

--- src/constants/pattens.js
+++ src/constants/pattens.js
@@ -1,6 +1,6 @@
 export const passwordPatten = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/
 
-export const addrPatten = /^0x[0-9a-z]{40}$/
+export const addrPatten = /^0x[0-9A-Za-z]{40}$/
 
 export const balancePatten = /^[1-9]([0-9]+)?(\.\d{1,8})?$|^[0](\.\d{1,8})$/
 
